ac313d43b4d350a890258984bff3d9b5a40f07b3
rmp22 <195054967+rmp22@users.noreply.github.com>
2021-11-12 11:25:02 +0000

    build: Override host metadata
    reference taken from
    https://grapheneos.social/@GrapheneOS/113870255397154703
    
    instead of fixed host number, use a random generated host number that follows google's host number format to bypass revolut's host checks
    
    Change-Id: I72feafd663a551eb0c6c4146d52137ed0dc7456d
    Co-authored-by: Terminator-J <terminator_j@hotbutteredmonkey.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1016,3 +1016,13 @@ function build_kernel() {
     chmod -x "${target_kernel_dir}/"*
     echo "Kernel build output copied to ${target_kernel_dir}/"
 }
+
+function generate_host_overrides() {
+    HEX=$(openssl rand -hex 8)
+    ALPHA=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1)
+    export BUILD_HOSTNAME="r-${HEX}-${ALPHA}"
+}
+
+generate_host_overrides
+
+export BUILD_USERNAME=android-build
